c6df8c5ffc44f92c5f9a0bb76b69955917eba4ac
sohailshams <sohailshamas@yahoo.com>
2021-03-18 16:31:46 +0100

    Added useEffect to protect user from going to preview without taking an image

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import { selectcameraImage } from '../features/cameraSlice';
 import './Preview.css';
 
 function Preview() {
   const cameraImage = useSelector(selectcameraImage);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace('/');
+    }
+  }, [cameraImage, history]);
   return (
     <div className="preview">
       <img src={cameraImage} alt="user image" />
