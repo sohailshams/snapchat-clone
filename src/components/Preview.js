a23f5ddbf5325f1e3c1ad96235c4eb64213bf70c
sohailshams <sohailshamas@yahoo.com>
2021-03-18 21:33:13 +0100

    Added icons and styled them

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,6 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { resetCameraImage, selectcameraImage } from '../features/cameraSlice';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
 import './Preview.css';
 
 function Preview() {
@@ -23,6 +31,15 @@ function Preview() {
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt="user pic" />
     </div>
   );
