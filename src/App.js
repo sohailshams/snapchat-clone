438ec4adffdaf786c42551d567eaaedc2310dd73
sohailshams <sohailshamas@yahoo.com>
2021-03-18 13:05:39 +0100

    Installed react-router-dom - imported Router, Switch, Route in app.js and updated return statement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import WebcamCapture from './components/WebcamCapture';
 
 function App() {
   return (
     <div className="app">
-      <WebcamCapture />
+      <Router>
+        <div className="app__body">
+          <Switch>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
