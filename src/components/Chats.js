0697d656fb7d4e3817a589b5dd65fd7d40a5022e
sohailshams <sohailshamas@yahoo.com>
2021-03-19 14:40:18 +0100

    Created Chats.js component and Chats.css file - added Avatar, input and chat bubble icon

---
+++ src/components/Chats.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import './Chats.css';
+
+function Chats() {
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <SearchIcon />
+          <input placeholder="Friends" type="text" />
+        </div>
+        <ChatBubbleIcon className="chats__chatIcon" />
+      </div>
+    </div>
+  );
+}
+
+export default Chats;
