33b7d1eeb2c69d9fbe3c97f44ddeec8e91488b45
sohailshams <sohailshamas@yahoo.com>
2021-03-30 10:29:19 +0200

    Defined a piece of state post & setPost - added useEffect to get the posts and update the post - map over the post and returned a Chat component & past few props

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import { db } from '../firebase';
 import './Chats.css';
+import Chat from './Chat';
 
 function Chats() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
   return (
     <div className="chats">
       <div className="chats__header">
@@ -15,7 +31,24 @@ function Chats() {
         </div>
         <ChatBubbleIcon className="chats__chatIcon" />
       </div>
-      <div className="chats__posts"></div>
+      <div className="chats__posts">
+        {posts.map(
+          ({
+            id,
+            data: { profilePic, username, timestamp, imageUrl, read },
+          }) => (
+            <Chat
+              key={id}
+              id={id}
+              username={username}
+              timestamp={timestamp}
+              imageUrl={imageUrl}
+              read={read}
+              profilePic={profilePic}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
