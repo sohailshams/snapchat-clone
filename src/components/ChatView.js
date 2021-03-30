16918cdfd55d923f681db6648214f9731530d21b
sohailshams <sohailshamas@yahoo.com>
2021-03-30 23:17:10 +0200

    Installed react-countdown-circle-timer, imported it in ChatView.js, added attributes and returned remaining time

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { selectSelectedImage } from '../features/appSlice';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import './ChatView.css';
 
 function ChatView() {
@@ -20,6 +21,21 @@ function ChatView() {
   return (
     <div className="chatView">
       <img src={selectedImage} onClick={exit} alt="" />
+      <CountdownCircleTimer
+        isPlaying
+        duration={10}
+        strokeWidth={6}
+        size={50}
+        colors={[
+          ['#004777', 0.33],
+          ['#F7B801', 0.33],
+          ['#A30000', 0.33],
+        ]}
+      >
+        {({ remainingTime }) => {
+          return remainingTime;
+        }}
+      </CountdownCircleTimer>
     </div>
   );
 }
