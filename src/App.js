a499bc42df86ab57f17b2a0efb207731c269cacb
sohailshams <sohailshamas@yahoo.com>
2021-03-30 22:49:16 +0200

    Creat ChatView component and ChatView.css - grab the selected image and history - added exit function on click and added useEffect to protect this view to be accessible only by clicking on the chat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Chats from './components/Chats';
+import ChatView from './components/ChatView';
 import Preview from './components/Preview';
 import WebcamCapture from './components/WebcamCapture';
 
@@ -11,6 +12,9 @@ function App() {
       <Router>
         <div className="app__body">
           <Switch>
+            <Route exact path="/chats/view">
+              <ChatView />
+            </Route>
             <Route exact path="/chats">
               <Chats />
             </Route>
---
+++ src/components/ChatView.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { selectSelectedImage } from '../features/appSlice';
+import './ChatView.css';
+
+function ChatView() {
+  const selectedImage = useSelector(selectSelectedImage);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!selectedImage) {
+      exit();
+    }
+  }, [selectedImage]);
+
+  const exit = () => {
+    history.replace('/chats');
+  };
+  return (
+    <div className="chatView">
+      <img src={selectedImage} onClick={exit} alt="" />
+    </div>
+  );
+}
+
+export default ChatView;
