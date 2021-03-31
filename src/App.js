8eee64473d4165a3410486c8801f1934e46d0d47
sohailshams <sohailshamas@yahoo.com>
2021-03-31 12:23:51 +0200

    Added a useEffect to make login persistenet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -7,11 +7,28 @@ import ChatView from './components/ChatView';
 import Login from './components/Login';
 import Preview from './components/Preview';
 import WebcamCapture from './components/WebcamCapture';
-import { selectUser } from './features/appSlice';
+import { login, logout, selectUser } from './features/appSlice';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  });
   return (
     <div className="app">
       <Router>
