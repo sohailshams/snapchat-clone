e26461d000e594bb4ecfec1231a947db2c8eee40
sohailshams <sohailshamas@yahoo.com>
2021-03-31 10:24:24 +0200

    Created Login component to display snapchat image and login button and added Login.css file for styles

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt="" />
+        <Button variant="outlined">Sign In</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
